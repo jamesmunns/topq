b90b7ae7c4675fcd570cd2e8298c5a02b14fb648
James Munns <james.munns@ferrous-systems.com>
2020-06-25 01:50:34 +0200

    Ready for first release

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Timeout Priority Queue
 
+[![Documentation](https://docs.rs/topq/badge.svg)](https://docs.rs/topq)
+
 This is a data structure that allows you to have multiple versions of data, with a timeout and priority level associated with each value.
 
 ## Example
@@ -22,43 +24,60 @@ let mut q: Topq<u32, u8, SomeTimer, U4> = Topq::new(timer);
 q.insert(10, 3, 30);
 
 // Get the top priority item
-assert_eq!(q.get(), Some(&10));
+assert_eq!(q.get_data(), Some(&10));
 
 // Insert the value "11", at priority level "4", valid for
 // "25" time units (the unit depends on the timer)
 q.insert(11, 4, 25);
 
 // Get the top priority item - it is now 11
-assert_eq!(q.get(), Some(&11));
+assert_eq!(q.get_data(), Some(&11));
 
 // Insert the value "12", at priority level "5", valid for
 // "20" time units (the unit depends on the timer)
 q.insert(12, 5, 20);
 
 // Get the top priority item - it is now 12
-assert_eq!(q.get(), Some(&12));
+assert_eq!(q.get_data(), Some(&12));
 
 // Insert the value "13", at priority level "6", valid for
 // "15" time units (the unit depends on the timer)
 q.insert(13, 6, 15);
-assert_eq!(q.get(), Some(&13));
+assert_eq!(q.get_data(), Some(&13));
 
 // Fast forward to time "15". 13 is still valid here
-assert_eq!(q.get(), Some(&13));
+assert_eq!(q.get_data(), Some(&13));
 
 // Fast forward to time "16". 13 has expired, so 12 is
 // now the highest priority + valid data
-assert_eq!(q.get(), Some(&12));
+assert_eq!(q.get_data(), Some(&12));
 
 // Fast forward to time "21". 12 has expired, so 11 is
 // now the highest priority + valid data
-assert_eq!(q.get(), Some(&11));
+assert_eq!(q.get_data(), Some(&11));
 
 // Fast forward to time "26". 11 has expired, so 10 is
 // now the highest priority + valid data
-assert_eq!(q.get(), Some(&10));
+assert_eq!(q.get_data(), Some(&10));
 
 // Fast forward to time "31". All items have expired,
 // so there is no available data
-assert_eq!(q.get(), None);
+assert_eq!(q.get_data(), None);
 ```
+
+# License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,44 @@
 #![cfg_attr(not(test), no_std)]
 
-use core::marker::PhantomData;
+use core::slice;
 use heapless::{ArrayLength, Vec};
 
+/// A trait that represents a (probably rolling) timer of arbitrary
+/// precision.
 pub trait Timer {
+    /// The type that is used to represent a time/offset
     type Time: Ord;
+
+    /// The number of ticks per second. e.g. if using a 32.786kHz
+    /// timer, this would be `32_768`
     const TICKS_PER_SECOND: u32;
 
+    /// Get the current time
     fn now(&self) -> Self::Time;
-    fn wrapping_add(left: &Self::Time, right: &Self::Time) -> Self::Time;
+
+    /// Add a timestamp plus an offset. When this type is expected to
+    /// be rolling (e.g. when using a `u32` with a 32.768kHz clock), the
+    /// addition should be done by wrapping
+    fn wrapping_add(time: &Self::Time, offset: &Self::Time) -> Self::Time;
 }
 
+/// A "Timeout Priority Queue"
+///
+/// This is generic over four parameters:
+///
+/// * D: The data type held by the priority queue
+/// * P: The priority type. Must implement `Ord`
+/// * T: The timer type. Must implement `topq::Timer`
+/// * N: The number of priority levels that can be kept at once
+///
+/// ## Note
+///
+/// `Topq` CAN handle timers that rollover periodically, however `Topq::purge()` MUST be called
+/// AT LEAST twice per rollover time period.
+///
+/// For example, when using a 32.768kHz clock source and a `u32` time value, the timer will
+/// roll over every 1.51 days or so. In this case, you MUST call `purge` to remove stale values
+/// at least every 0.75 days or so.
 pub struct Topq<D, P, T, N>
 where
     D: 'static,
@@ -18,7 +46,7 @@ where
     T: Timer,
     N: ArrayLength<TopqItem<D, P, T>>,
 {
-    pub queue: Vec<TopqItem<D, P, T>, N>,
+    queue: Vec<TopqItem<D, P, T>, N>,
     timer: T,
 }
 
@@ -29,6 +57,7 @@ where
     T: Timer,
     N: ArrayLength<TopqItem<D, P, T>>,
 {
+    /// Create an empty Topq with the given timer
     pub fn new(timer: T) -> Self {
         Self {
             queue: Vec::new(),
@@ -36,6 +65,17 @@ where
         }
     }
 
+    /// Get the current time based on the internal counter
+    pub fn now(&self) -> T::Time {
+        self.timer.now()
+    }
+
+    /// Insert a datapoint into the priority queue
+    ///
+    /// If the queue already contains an item with the same priority, the old
+    /// data and timeout will be replaced. If the queue does not contain an item
+    /// at this priority, it will be inserted if there is room or it is a higher
+    /// priority than the existing items
     pub fn insert(&mut self, item: D, prio: P, valid_for: T::Time) {
         let now = self.timer.now();
         let exp = T::wrapping_add(&now, &valid_for);
@@ -43,7 +83,6 @@ where
         let new_item = TopqItem {
             item,
             prio,
-            timer: PhantomData,
             start_time: now,
             expiry_time: exp,
         };
@@ -83,6 +122,9 @@ where
         }
     }
 
+    /// Remove any expired items from the priority queue
+    ///
+    /// See the module level documentation for when it is necessary to call this function
     pub fn prune(&mut self) {
         // TODO: Do this without making a second queue (remove items and shift up as needed),
         // or having to re-sort the queue because we popped everything backwards
@@ -104,14 +146,27 @@ where
         self.queue = new;
     }
 
-    pub fn get(&self) -> Option<&D> {
+    /// Obtain the highest priority and currently valid data, if any
+    ///
+    /// This is typically used when you ONLY need the current value, and not
+    /// the remaining validity time or the priority of the currently valid data
+    pub fn get_data(&self) -> Option<&D> {
+        self.get_item()
+            .map(|i| &i.item)
+    }
+
+    /// Obtain the highest priority and currently valid topq item, if any
+    ///
+    /// This is typically used when you need the current value, AND ALSO need
+    /// the remaining validity time or the priority of the currently valid data
+    pub fn get_item(&self) -> Option<&TopqItem<D, P, T>> {
         let now = self.timer.now();
         self.queue
             .iter()
             .find(|item| item.valid_at_time(&now))
-            .map(|i| &i.item)
     }
 }
+
 
 #[derive(Debug)]
 pub struct TopqItem<D, P, T>
@@ -120,11 +175,10 @@ where
     P: Ord,
     T: Timer,
 {
-    item: D,
-    prio: P,
-    timer: PhantomData<T>,
-    start_time: T::Time,
-    expiry_time: T::Time,
+    pub item: D,
+    pub prio: P,
+    pub start_time: T::Time,
+    pub expiry_time: T::Time,
 }
 
 impl<D, P, T> TopqItem<D, P, T>
@@ -141,6 +195,37 @@ where
             // Rollover case
             *time >= self.start_time || *time <= self.expiry_time
         }
+    }
+}
+
+
+impl<'a, D, P, T, N> IntoIterator for &'a Topq<D, P, T, N>
+where
+    D: 'static,
+    P: Ord,
+    T: Timer,
+    N: ArrayLength<TopqItem<D, P, T>>,
+{
+    type Item = &'a TopqItem<D, P, T>;
+    type IntoIter = slice::Iter<'a, TopqItem<D, P, T>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.queue.iter()
+    }
+}
+
+impl<'a, D, P, T, N> IntoIterator for &'a mut Topq<D, P, T, N>
+where
+    D: 'static,
+    P: Ord,
+    T: Timer,
+    N: ArrayLength<TopqItem<D, P, T>>,
+{
+    type Item = &'a mut TopqItem<D, P, T>;
+    type IntoIter = slice::IterMut<'a, TopqItem<D, P, T>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.queue.iter_mut()
     }
 }
 
@@ -181,25 +266,25 @@ mod test {
         use PrioEnum::*;
 
         q.insert(10, Low, 30);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         q.insert(11, Medium, 25);
-        assert_eq!(q.get(), Some(&11));
+        assert_eq!(q.get_data(), Some(&11));
 
         q.insert(12, High, 20);
-        assert_eq!(q.get(), Some(&12));
+        assert_eq!(q.get_data(), Some(&12));
 
         TIMER.store(20, SeqCst);
-        assert_eq!(q.get(), Some(&12));
+        assert_eq!(q.get_data(), Some(&12));
 
         TIMER.store(21, SeqCst);
-        assert_eq!(q.get(), Some(&11));
+        assert_eq!(q.get_data(), Some(&11));
 
         TIMER.store(26, SeqCst);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         TIMER.store(31, SeqCst);
-        assert_eq!(q.get(), None);
+        assert_eq!(q.get_data(), None);
     }
 
     #[test]
@@ -209,31 +294,31 @@ mod test {
         let mut q: Topq<u32, u8, FakeTimer, U4> = Topq::new(timer);
 
         q.insert(10, 3, 30);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         q.insert(11, 4, 25);
-        assert_eq!(q.get(), Some(&11));
+        assert_eq!(q.get_data(), Some(&11));
 
         q.insert(12, 5, 20);
-        assert_eq!(q.get(), Some(&12));
+        assert_eq!(q.get_data(), Some(&12));
 
         q.insert(13, 6, 15);
-        assert_eq!(q.get(), Some(&13));
+        assert_eq!(q.get_data(), Some(&13));
 
         TIMER.store(15, SeqCst);
-        assert_eq!(q.get(), Some(&13));
+        assert_eq!(q.get_data(), Some(&13));
 
         TIMER.store(16, SeqCst);
-        assert_eq!(q.get(), Some(&12));
+        assert_eq!(q.get_data(), Some(&12));
 
         TIMER.store(21, SeqCst);
-        assert_eq!(q.get(), Some(&11));
+        assert_eq!(q.get_data(), Some(&11));
 
         TIMER.store(26, SeqCst);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         TIMER.store(31, SeqCst);
-        assert_eq!(q.get(), None);
+        assert_eq!(q.get_data(), None);
     }
 
     #[test]
@@ -243,31 +328,35 @@ mod test {
         let mut q: Topq<u32, u8, FakeTimer, U4> = Topq::new(timer);
 
         q.insert(10, 3, 30);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         q.insert(13, 6, 15);
-        assert_eq!(q.get(), Some(&13));
+        assert_eq!(q.get_data(), Some(&13));
 
         q.insert(12, 5, 20);
-        assert_eq!(q.get(), Some(&13));
+        assert_eq!(q.get_data(), Some(&13));
 
         q.insert(11, 4, 25);
-        assert_eq!(q.get(), Some(&13));
+        assert_eq!(q.get_data(), Some(&13));
+
+        q.into_iter().for_each(|t| {
+            println!("{:?}", t);
+        });
 
         TIMER.store(15, SeqCst);
-        assert_eq!(q.get(), Some(&13));
+        assert_eq!(q.get_data(), Some(&13));
 
         TIMER.store(16, SeqCst);
-        assert_eq!(q.get(), Some(&12));
+        assert_eq!(q.get_data(), Some(&12));
 
         TIMER.store(21, SeqCst);
-        assert_eq!(q.get(), Some(&11));
+        assert_eq!(q.get_data(), Some(&11));
 
         TIMER.store(26, SeqCst);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         TIMER.store(31, SeqCst);
-        assert_eq!(q.get(), None);
+        assert_eq!(q.get_data(), None);
     }
 
     #[test]
@@ -278,21 +367,21 @@ mod test {
 
         TIMER.store(0xFFFF_FFF0, SeqCst);
         q.insert(10, 3, 32);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         TIMER.store(0xFFFF_FFF8, SeqCst);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         TIMER.store(0xFFFF_FFFF, SeqCst);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         TIMER.store(0x0000_0000, SeqCst);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         TIMER.store(0x0000_0010, SeqCst);
-        assert_eq!(q.get(), Some(&10));
+        assert_eq!(q.get_data(), Some(&10));
 
         TIMER.store(0x0000_0011, SeqCst);
-        assert_eq!(q.get(), None);
+        assert_eq!(q.get_data(), None);
     }
 }
